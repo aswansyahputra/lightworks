64f7f01a7dd402acff41a313363261121ee0bc09
Muhammad Aswan Syahputra <muhammadaswansyahputra@gmail.com>
2020-02-06 11:07:44 +0700

    add answer for lw2

--- script/02_messy-excel.R
+++ script/02_messy-excel.R
@@ -25,3 +25,19 @@ glimpse(teachers)
 # 5. Menghapus kolom yang tidak diperlukan
 # 6. Identifikasi observasi duplikat berdasarkan nama depan dan nama belakang
 # 7. Menghapus observasi duplikat (opsional)
+
+clean_data <- 
+  dirty_data %>% 
+  remove_empty(c("rows", "cols")) %>% 
+  clean_names(case = "snake") %>% 
+  mutate(
+    hire_date = excel_numeric_to_date(hire_date),
+    cert = coalesce(certification_9, certification_10)
+  ) %>% 
+  select(-starts_with("certi"))
+
+clean_data %>%   
+  get_dupes(first_name, last_name)
+
+clean_data %>%   
+  distinct(first_name, last_name, .keep_all = TRUE)
